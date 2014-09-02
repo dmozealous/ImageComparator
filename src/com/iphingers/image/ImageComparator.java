ec2672026ade45be490b3126c049d216aff499b0
Dave Mozealous <dave@mozealous.com>
2014-09-01 19:02:25 -0700

    fixed bugs, added support for comparing 2 different sized images

--- src/com/iphingers/image/ImageComparator.java
+++ src/com/iphingers/image/ImageComparator.java
@@ -1,5 +1,8 @@
 package com.iphingers.image;
 
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Rectangle;
 import java.awt.image.BufferedImage;
 import java.awt.image.ColorModel;
 import java.awt.image.WritableRaster;
@@ -27,6 +30,8 @@ public class ImageComparator {
 	/** The list of pixels that differ between images. */
 	List<Pixel> pixels;
 	
+	Rectangle newRect;
+	
 	/**
 	 * Instantiates a new image comparator.
 	 *
@@ -34,10 +39,24 @@ public class ImageComparator {
 	 * @param img2 the img2
 	 */
 	public ImageComparator(BufferedImage img1, BufferedImage img2){
+		
 		this.img1 = img1;
 		this.img2 = img2;
 		
 		pixels = new ArrayList<Pixel>();
+		
+		newRect = createCompositeRectangle(img1,img2);
+	}
+	
+	private Rectangle createCompositeRectangle(BufferedImage img1, BufferedImage img2){
+		
+		int xMax;
+		int yMax;
+		
+		xMax = Math.max(img1.getWidth(), img2.getWidth());
+		yMax = Math.max(img1.getHeight(), img2.getHeight());
+		
+		return new Rectangle(xMax,yMax);
 	}
 
 	/**
@@ -51,19 +70,21 @@ public class ImageComparator {
 			return diffCount;
 		}
 		
-		for(int y = 0; y < img1.getHeight(); y++){
+		for(int y = 0; y < img1.getHeight() && y < img2.getHeight(); y++){
 			
-			for(int x = 0; x < img1.getWidth(); x++){
+			for(int x = 0; x < img1.getWidth() && x < img2.getWidth(); x++){
 				
-				if(img1.getRGB(x, y) != img2.getRGB(x, y)){
-					Pixel pixel = new Pixel(x,y);
-					pixels.add(pixel);
-					diffCount++;
-				}
-
+					if(img1.getRGB(x, y) != img2.getRGB(x, y)){
+						Pixel pixel = new Pixel(x,y);
+						pixels.add(pixel);
+						diffCount++;
+					}
+				
 			}
 	
 		}
+		
+		diffCount = diffCount + (getWidthDiff() * getHeightDiff());
 		
 		return diffCount;
 	}
@@ -74,7 +95,7 @@ public class ImageComparator {
 	 * @return the width diff
 	 */
 	public int getWidthDiff() {
-		return img1.getWidth() - img2.getWidth();
+		return Math.abs(img1.getWidth() - img2.getWidth());
 	}
 
 	/**
@@ -83,7 +104,7 @@ public class ImageComparator {
 	 * @return the height diff
 	 */
 	public int getHeightDiff() {
-		return img1.getHeight() - img2.getHeight();
+		return Math.abs(img1.getHeight() - img2.getHeight());
 	}
 	
 	/**
@@ -91,24 +112,30 @@ public class ImageComparator {
 	 *
 	 * @return the buffered image
 	 */
-	public BufferedImage createCompositeImage(){
+	public BufferedImage createCompositeImage(File outputFile){
+		
+		int widthDiff = getWidthDiff();
+		int heightDiff = getHeightDiff();
+		int width = (int)newRect.getWidth();
+		int height = (int)newRect.getHeight();
 		
 		if(getPixelDiffCount() == 0){
 			return img1;
 		}
 		
-		ColorModel cm = img1.getColorModel();
-		boolean isAlphaPremultiplied = cm.isAlphaPremultiplied();
-		WritableRaster raster = img1.copyData(null);
-		BufferedImage newImage = new BufferedImage(cm, raster, isAlphaPremultiplied, null);
+		BufferedImage newImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+		Graphics g = newImage.getGraphics();
+		g.setColor(Color.red);
+		g.drawImage(img1, 0, 0, null);
+		g.fillRect(width - widthDiff, 0, widthDiff, Math.min(img1.getHeight(), img2.getHeight()));
+		g.fillRect(0, height - heightDiff, width, height);
 		
 		for(Pixel pixel : pixels){
 			newImage.setRGB(pixel.getX(), pixel.getY(), 16711680);
 		}
 		
-	    File outputfile = new File("Images/new.png");
 	    try {
-			ImageIO.write(newImage, "png", outputfile);
+			ImageIO.write(newImage, "png", outputFile);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
--- src/com/iphingers/tst/comparatorTests.java
+++ src/com/iphingers/tst/comparatorTests.java
@@ -15,24 +15,34 @@ import com.iphingers.image.ImageComparator;
 
 public class comparatorTests {
 	
-	private final String IMAGE1 = "Images/IMG_4776.png";
-	private final String IMAGE2 = "Images/IMG_4777.png";
-	private final String IMAGE3 = "Images/IMG_4778.png";
-	private final String IMAGE1SAME = "Images/IMG_4776.png";
-	private BufferedImage img1;
-	private BufferedImage img2;
-	private BufferedImage img3;
-	private BufferedImage imgSame;
+	private final File BASEIMAGE = new File("Images/IMG_4776.png");
+	private final File BASEIMAGE2 = new File("Images/IMG_4777.png");
+	private final File BASEIMAGETIMEDIFF = new File("Images/IMG_4778.png");
+	private final File BASEIMAGESMALLER = new File("Images/Smaller.png");
+	private final File BASEIMAGEBIGGER = new File("Images/Bigger.png");
+	private BufferedImage baseImage;
+	private BufferedImage baseImage2;
+	private BufferedImage baseImageTimeDiff;
+	private BufferedImage baseImageSame;
+	private BufferedImage baseImageSmaller;
+	private BufferedImage baseImageBigger;
+	private BufferedImage imgGood;	
+	private File GOODFILECOMP = new File("Images/Good_COMP.png");
+	private File SMALLERFILECOMP = new File("Images/Smaller_COMP.png");
+	private File BIGGERFILECOMP = new File("Images/Bigger_COMP.png");
 	
 	@Before
 	public void beforeTest(){
 		
 		try {
 			
-			img1 = ImageIO.read(new File(IMAGE1));
-			img2 = ImageIO.read(new File(IMAGE2));
-			img3 = ImageIO.read(new File(IMAGE3));
-			imgSame = ImageIO.read(new File(IMAGE1SAME));
+			baseImage = ImageIO.read(BASEIMAGE);
+			baseImage2 = ImageIO.read(BASEIMAGE2);
+			baseImageTimeDiff = ImageIO.read(BASEIMAGETIMEDIFF);
+			baseImageSame = ImageIO.read(BASEIMAGE);		
+			imgGood = ImageIO.read(GOODFILECOMP);
+			baseImageSmaller = ImageIO.read(BASEIMAGESMALLER);
+			baseImageBigger = ImageIO.read(BASEIMAGEBIGGER);
 			
 		} catch (IOException e) {
 			e.printStackTrace();
@@ -43,7 +53,7 @@ public class comparatorTests {
 	@Test
 	public void testSame() {
 		
-		ImageComparator comp = new ImageComparator(img1, imgSame);
+		ImageComparator comp = new ImageComparator(baseImage, baseImageSame);
 		assertEquals(0, comp.getWidthDiff());
 		assertEquals(0, comp.getHeightDiff());
 		assertEquals(0, comp.getPixelDiffCount());
@@ -52,7 +62,7 @@ public class comparatorTests {
 	@Test
 	public void testDiffSameDimensions() {
 		
-		ImageComparator comp = new ImageComparator(img1, img2);
+		ImageComparator comp = new ImageComparator(baseImage, baseImage2);
 		assertEquals(0, comp.getWidthDiff());
 		assertEquals(0, comp.getHeightDiff());
 		assertEquals(229721, comp.getPixelDiffCount());
@@ -62,7 +72,7 @@ public class comparatorTests {
 	@Test
 	public void testDiffSameDimensionsPercent() {
 		
-		ImageComparator comp = new ImageComparator(img1, img2);
+		ImageComparator comp = new ImageComparator(baseImage, baseImage2);
 		assertEquals(0, comp.getWidthDiff());
 		assertEquals(0, comp.getHeightDiff());
 		assertEquals(31, comp.getPixelDiffPercent(),1);
@@ -71,10 +81,30 @@ public class comparatorTests {
 	@Test
 	public void testCreateComposite() {
 		
-		ImageComparator comp = new ImageComparator(img1, img3);
-		BufferedImage composite = comp.createCompositeImage();
-		ImageComparator compNew = new ImageComparator(img1, composite);
+		ImageComparator comp = new ImageComparator(baseImage, baseImageTimeDiff);
+		BufferedImage composite = comp.createCompositeImage(GOODFILECOMP);
+		ImageComparator compNew = new ImageComparator(baseImage, composite);
 		assertEquals(comp.getPixelDiffCount(), compNew.getPixelDiffCount());
 
+	}
+	
+	@Test
+	public void testDiffDimensionsSmaller() {
+		
+		ImageComparator comp = new ImageComparator(baseImage, baseImageSmaller);
+		assertEquals(100, comp.getWidthDiff());
+		assertEquals(100, comp.getHeightDiff());
+		assertEquals(10000, comp.getPixelDiffCount());
+		comp.createCompositeImage(SMALLERFILECOMP);
+	}
+	
+	@Test
+	public void testDiffDimensionsBigger() {
+		
+		ImageComparator comp = new ImageComparator(baseImage, baseImageBigger);
+		assertEquals(100, comp.getWidthDiff());
+		assertEquals(100, comp.getHeightDiff());
+		assertEquals(10000, comp.getPixelDiffCount());
+		comp.createCompositeImage(BIGGERFILECOMP);
 	}
 }
